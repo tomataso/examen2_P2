4ec6db5571c0a8aa3ed9ad7f64cccaf6df90204a
Juan Carlos <tomatejuan@hotmail.com>
2021-04-20 14:01:51 -0600

    Control Extensions

--- ProgressBarExamen2/ProgressBarExam/Models/Controls/CtrlProgressBar.cs
+++ ProgressBarExamen2/ProgressBarExam/Models/Controls/CtrlProgressBar.cs
@@ -10,7 +10,19 @@ namespace ProgressBarExam.Models.Controls
 
 
 
+      //  public string Id { get; set; }
 
+        // Aca van coordenadas de SJ Costa Rica Quemadas.
+        public string Porcentaje { get; set; } = "50%";
+        public string Color { get; set; } = "#FF0000";
+        // El Color aca hexagecimal es Rojo.
+        
+
+
+        public CtrlProgressBar()
+        {
+            ViewName = "";
+        }
 
 
 
---
+++ ProgressBarExamen2/ProgressBarExam/Models/Helpers/ControlExtensions.cs
@@ -0,0 +1,29 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProgressBarExam.Models.Controls;
+
+ namespace ProgressBarExam.Models.Helpers
+{
+    public static class ControlExtensions
+    {
+
+        public static HtmlString CtrlProgressBar(this HtmlHelper html, string id, string pPorcentaje , string pColor)
+        {
+            var ctrl = new CtrlProgressBar
+            {
+                Id = id,
+                Porcentaje = pPorcentaje,
+                Color = pColor
+            }; ;
+
+
+            return new HtmlString(ctrl.GetHtml());
+        }
+
+    }
+}
