e07e63c946ed98472e3b4d102fd0188ef1b74649
Juan Carlos <tomatejuan@hotmail.com>
2021-04-20 13:16:49 -0600

    Fix Base Model

--- ProgressBarExamen2/ProgressBarExam/Models/Controls/CtrlBaseModel.cs
+++ ProgressBarExamen2/ProgressBarExam/Models/Controls/CtrlBaseModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 
@@ -41,5 +42,4 @@ namespace ProgressBarExam.Models.Controls
         }
     }
 
-}
 }
